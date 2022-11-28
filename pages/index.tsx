2a9dd53015d65feedcb882425edf3d97b3761577
Vishnu <vishnu@Vishnus-MacBook-Pro.local>
2022-11-29 01:52:54 +0530

    init flowchart

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,6 +19,18 @@ export default function Home() {
     },
   ];
 
+  const flowChartData = [
+    { src: "/logo.png", description: "Too many students and courses" },
+    {
+      src: "/logo.png",
+      description: "Intelligent Analytics of popular roadmaps",
+    },
+    {
+      src: "/logo.png",
+      description: "Better resource allocation and management",
+    },
+  ];
+
   return (
     <div className="lg:px-18 px-4 py-16 sm:px-16 md:px-32 xl:px-32 ">
       <Hero
@@ -28,17 +40,41 @@ export default function Home() {
         displaySwitch={false}
       />
 
-      <div>
+      <div className="mt-16 space-y-8">
         {content.map((item, index) => (
           <div
-            className={`lg:flex ${index % 2 !== 0 ? "flex-row-reverse" : ""}`}
+            className={`items-center justify-center gap-16 lg:flex ${
+              index % 2 !== 0 ? "flex-row-reverse" : ""
+            }`}
             key={index}
           >
-            <div className="lg:w-[50vw]">
+            <div className="lg:max-w-[40vw]">
               <h1 className="font-semibold ">{item.title}</h1>
               <p className="text-gray-600">{item.description}</p>
             </div>
-            <Image src="/" width={100} height={100} alt="" className="flex-1" />
+            <Image
+              src="/logo.png"
+              width={100}
+              height={100}
+              alt=""
+              className="h-auto w-full lg:max-w-[20vw]"
+            />
+          </div>
+        ))}
+      </div>
+
+      {/* flow chart */}
+      {/* todo : modularise */}
+      <div className="mt-8 flex justify-center gap-16">
+        {flowChartData.map((item, index) => (
+          <div
+            key={index}
+            className={`flex flex-col items-center ${
+              index % 2 != 0 ? "flex-col-reverse" : ""
+            }`}
+          >
+            <p>{item.description}</p>
+            <Image src={item.src} width={100} height={100} alt="" />
           </div>
         ))}
       </div>
